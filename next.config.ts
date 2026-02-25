d161ef44709dea6e089743b40cf44a665a6bd73a
ghelg <95490983+guillaume-helg@users.noreply.github.com>
2026-02-25 18:47:04 +0100

    feat: update projects part (#16)

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const isDev = process.env.NODE_ENV === "development";
 const isGithubActions = process.env.GITHUB_ACTIONS === "true";
 const repoName = "/portfolio_f3nris";
 
-// On GitHub Actions, we ALWAYS want the basePath, even if NODE_ENV isn't "production" yet
 const basePath = (isDev && !isGithubActions) ? "" : repoName;
 
 console.log(`[NextConfig] isDev: ${isDev}, isGithubActions: ${isGithubActions}, basePath: "${basePath}"`);
